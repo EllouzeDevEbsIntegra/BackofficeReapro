c1491059570fecefce25d14f69032215508485ac
Med Ali <ellouze@sopiq.tn>
2026-01-22 14:41:15 +0100

    V22.01.2026

--- src/main/java/com/reapro/achat/Controller/AuthController.java
+++ src/main/java/com/reapro/achat/Controller/AuthController.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/auth")
+@RequestMapping({"api/auth", "auth"})
 @RequiredArgsConstructor
 public class AuthController {
 
--- src/main/java/com/reapro/achat/config/SecurityConfig.java
+++ src/main/java/com/reapro/achat/config/SecurityConfig.java
@@ -36,7 +36,8 @@ public class SecurityConfig {
                                 "/api/auth/refresh-token",
                                 "/api/auth/forgot-password",
                                 "/api/auth/verify-reset-code",
-                                "/api/auth/change-password"
+                                "/api/auth/change-password",
+                                "/auth/refresh-token"
                         ).permitAll()
 
                         // Toutes les autres routes → protégées
